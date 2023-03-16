4882f27cd08ac239085e9b5bad141b3e617bb5e9
victor.baggio <baggiobaggiovictor@gmail.com>
2023-03-16 17:03:26 -0300

    refactor: refatorando a service de get movies com boas praticas

--- src/app/config/movies-config.service.ts
+++
@@ -1,9 +0,0 @@
-interface IURL {
-  movies: string;
-}
-
-export class MoviesConfigService {
-  public static readonly urls: Readonly<IURL> = {
-    movies: 'https://api.themoviedb.org/3/',
-  };
-}
--- src/app/core/services/get-movies.service.ts
+++ src/app/core/services/get-movies.service.ts
@@ -2,6 +2,11 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'environment';
+import { HttpResponse } from '../interfaces/http-reponse.interface';
+
+interface Movie {
+  name: string;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +14,9 @@ import { environment } from 'environment';
 export class GetMoviesService {
   private http = inject(HttpClient);
 
-  getMoviesTrending(): Observable<any[]> {
-    return this.http.get<any[]>(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${environment.apiKey}`
+  getMoviesTrending(): Observable<HttpResponse<Movie[]>> {
+    return this.http.get<HttpResponse<Movie[]>>(
+      `${environment.baseApiUrl}/trending/all/day?api_key=${environment.apiKey}`
     );
   }
 }
